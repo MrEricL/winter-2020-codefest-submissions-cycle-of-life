8142ebac7daa8cf511eb339315e10fd3aecb6124
Khinshan Khan <khanshan501@gmail.com>
2020-01-24 12:41:05 -0500

    capturing output successfully of process

--- api-endpoint/app.py
+++ api-endpoint/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect, session
+import os
+import subprocess
 
 app = Flask(__name__)
 
+DIRNAME = os.path.dirname(__file__)
+PROCESSNAME = os.path.join(DIRNAME, '..', 'mock-process', 'hello.py')
 
 @app.route("/")
 def index():
-    # return render_template("index.html")
-    return "hello"
+    p = subprocess.Popen('python ' + PROCESSNAME, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = []
+    for line in p.stdout.readlines():
+        output.append(line.decode().strip())
+    retval = p.wait()
+    return "".join(output)
 
 
 
